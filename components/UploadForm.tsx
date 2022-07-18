9ba81d41b21953e5de493b22dc1abb7c8f1f73eb
Wes-Isaac <yisehakmitiku@gmail.com>
2022-07-18 16:12:41 +0800

    Create uploadform

---
+++ components/UploadForm.tsx
@@ -0,0 +1,14 @@
+const UploadForm = () => {
+  return (
+    <form>
+      <input type="file" />
+      <label htmlFor="title">Title:</label>
+      <input type="text" id="title" placeholder="Title" />
+      <label htmlFor="price">Price:</label>
+      <input type="text" id="price" placeholder="Price" />
+    </form>
+  )
+}
+
+export default UploadForm;
+ 
---
+++ pages/add.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from "next";
+import UploadForm from "../components/UploadForm";
+
+const add:NextPage = () => {
+  return(
+    <div>
+      <h1>Upload Form</h1>
+      <UploadForm />
+    </div>
+  )
+
+}
+
+export default add;

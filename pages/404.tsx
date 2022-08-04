b241399a4faafed944c06939aba7eed80eb574cc
Wes-Isaac <yisehakmitiku@gmail.com>
2022-08-04 17:07:23 +0800

    Style custom 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,17 +2,18 @@ import Link from 'next/link';
 
 export default function Custom404() {
   return (
-    <main>
-      <h1>404 - That page does not seem to exist...</h1>
+    <main className='m-4'>
+      <h1 className='my-4 text-lg font-semibold'>404 - That page does not seem to exist...</h1>
       <iframe
+        className='mx-auto'
         src="https://giphy.com/embed/l2JehQ2GitHGdVG9y"
-        width="480"
+        width="400"
         height="362"
         frameBorder="0"
         allowFullScreen
       ></iframe>
       <Link href="/">
-        <button className="btn-blue">Go home</button>
+        <button className="m-2 p-1 text-lg cursor-pointer bg-white font-semibold border-2 border-black rounded-tr-lg">Go home</button>
       </Link>
     </main>
   );
